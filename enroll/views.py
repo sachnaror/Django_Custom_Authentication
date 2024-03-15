689be793d9f93786eb2dbe3952a3f154b30acc02
Sachin <sachnaror@gmail.com>
2024-03-15 20:36:56 +0530

    typing commit message is most boring part

--- enroll/views.py
+++ enroll/views.py
@@ -19,3 +19,74 @@ def sign_up(request):
     else:
         fm = SignUpForm()
     return render(request, 'enroll/signup.html', {'form': fm})
+
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                user_name = fm.cleaned_data['username']
+                user_password = fm.cleaned_data['password']
+                user = authenticate(username=user_name, password=user_password)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in successfully !!')
+                    return HttpResponseRedirect('/profile/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html', {'form': fm})
+    else:
+        return HttpResponseRedirect('/profile/')
+
+
+def user_profile(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if request.user.is_superuser:
+                fm = EditAdminProfileForm(request.POST, instance=request.user)
+            else:
+                fm = EditUserProfileForm(request.POST, instance=request.user)
+            if fm.is_valid():
+                messages.success(request, 'Profile Updated')
+                fm.save()
+        else:
+            if request.user.is_superuser:
+                fm = EditAdminProfileForm(instance=request.user)
+            else:
+                fm = EditUserProfileForm(instance=request.user)
+        return render(request, 'enroll/profile.html', {'name': request.user, 'form': fm})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+
+def user_change_password2(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            fm = SetPasswordForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                # Update user session
+                update_session_auth_hash(request, fm.user)
+                messages.success(request, 'Password changed successfully')
+                return HttpResponseRedirect('/profile/')
+        else:
+            fm = SetPasswordForm(user=request.user)
+        return render(request, 'enroll/changePassword2.html', {'form': fm})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def user_detail(request, id):
+    if request.user.is_authenticated:
+        pi = User.objects.get(pk=id)
+        fm = EditAdminProfileForm(instance=pi)
+        return render(request, 'enroll/userdetail.html',
+                      {'name': request.user.username, 'form': fm})
+    else:
+        return HttpResponseRedirect('/login/')
