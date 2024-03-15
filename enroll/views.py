f8c0dcc2be320ef8c1459e104334f9718aae030b
Sachin <sachnaror@gmail.com>
2024-03-15 19:00:20 +0530

    typing commit message is most boring part

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import (authenticate, login, logout,
+                                 update_session_auth_hash)
+from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm,
+                                       SetPasswordForm, UserChangeForm)
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponseRedirect, render
 
-# Create your views here.
+from .forms import EditAdminProfileForm, EditUserProfileForm, SignUpForm
+
+
+def sign_up(request):
+    if request.method == "POST":
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            messages.success(request, 'Account created successfully')
+            fm.save()
+            return HttpResponseRedirect('/login/')
+    else:
+        fm = SignUpForm()
+    return render(request, 'enroll/signup.html', {'form': fm})
